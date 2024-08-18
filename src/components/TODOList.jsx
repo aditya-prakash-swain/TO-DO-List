03c00545bbb2d58392ece3aa3ca0897ccf5ec78d
Aditya <2002adityaprakash@gmail.com>
2024-08-19 00:58:18 +0530

    ui modified

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -9,15 +9,15 @@ function TODOList({ todos, setTodos }) {
   }
 
   function addTask() {
-    if (newTask.trim() !== null) {
+    if (newTask.trim() !== "") {
       setTasks((t) => [...t, newTask]);
       setNewTask("");
     }
   }
 
   function deleteTask(index) {
-    const updatedTask= tasks.filter((_,i)=> i!== index)
-    setTasks(updatedTask)
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTask);
   }
   return (
     <div className="to-do-list">
@@ -29,16 +29,22 @@ function TODOList({ todos, setTodos }) {
           placeholder="enter some task"
           onChange={handleInputChange}
         />
+
         <button className="add-button" onClick={addTask}>
-          Add
+          <span className="transition"></span>
+          <span className="gradient"></span>
+          <span className="label">Add</span>
         </button>
       </div>
       <ol>
         {tasks.map((task, index) => (
           <li key={index}>
             <span className="text">{task}</span>
-            <button className="delete-button" onClick={() => deleteTask(index)}>
-              Delete
+
+            <button className="button" onClick={() => deleteTask(index)}>
+              <svg viewBox="0 0 448 512" class="svgIcon">
+                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+              </svg>
             </button>
           </li>
         ))}
